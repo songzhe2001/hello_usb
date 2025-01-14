fe808200853b086cb00616f9e9247dc6b394d847
songzhe2001 <83053537+songzhe2001@users.noreply.github.com>
2025-01-14 14:08:34 +0800

    new file:   .gitignore 	new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/cmake-kits.json 	new file:   .vscode/extensions.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   CMakeLists.txt 	new file:   hello_usb.c 	new file:   pico_sdk_import.cmake 	new file:   tmf882x_image.c 	new file:   tmf882x_image.h

---
+++ tmf882x_image.h
@@ -0,0 +1,10 @@
+#ifndef TMF882X_IMAGE_H
+#define TMF882X_IMAGE_H
+
+extern const unsigned long tmf882x_image_termination;
+extern const unsigned long tmf882x_image_start;
+extern const unsigned long tmf882x_image_finish;
+extern const unsigned long tmf882x_image_length;
+extern const unsigned char tmf882x_image[];
+
+#endif /* TMF882X_IMAGE_H */
